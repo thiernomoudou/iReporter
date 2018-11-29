5716458742336934a2b6c98b285709932fa4c605
Thierno Souleymane Diallo <souleymanemoudou@gmail.com>
2018-11-29 11:31:41 +0000

    Remove duplicate code and add a user test case

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-return-assign */
 /* eslint-disable no-lonely-if */
 
-import { usersData, incidentsData } from '../database';
+import { incidentsData } from '../database';
+
 
 /**
  * Controller to handle all users endpoint routes
@@ -17,13 +18,15 @@ class UsersController {
    */
 
   getAllUserIncidents(req, res) {
-    // getting the id of the user
-    const userId = parseInt(req.params.id, 10);
+    // From the userMiddleware
+    const userId = req.user.id;
+
     const userIncidents = incidentsData.filter(item => item.createdBy === userId);
+    
     if (userIncidents.length === 0) {
       res.status(404).send({
         status: 404,
-        error: 'User not found'
+        error: 'User does not have redflags or interventions yet'
       });
     } else {
       res.send({ status: 200, data: userIncidents });
@@ -39,17 +42,9 @@ class UsersController {
  */
 
   getUserProfile(req, res) {
-    // getting the id of the user
-    const userId = parseInt(req.params.id, 10);
-    const userProfile = usersData.filter(item => item.id === userId);
-    if (userProfile.length === 0) {
-      res.status(404).send({
-        status: 404,
-        error: 'User not found'
-      });
-    } else {
-      res.send({ status: 200, data: userProfile });
-    }
+    // getting the user from the userMiddleware
+    const userProfile = req.user;
+    res.send({ status: 200, data: [userProfile] });
   }
 }
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import UsersController from '../controllers/usersController';
+import userMiddleware from '../middleware/userMiddleware';
 // import authMiddleware from '../middleware/authenticate';
 
 const usersRoutes = new Router();
 const usersController = new UsersController();
 
 // get all incidents for an individual user
-usersRoutes.get('/:id/incidents', usersController.getAllUserIncidents);
+usersRoutes.get('/:id/incidents', userMiddleware, usersController.getAllUserIncidents);
 
 // get a user profile
-usersRoutes.get('/:id/profile', usersController.getUserProfile);
+usersRoutes.get('/:id/profile', userMiddleware, usersController.getUserProfile);
 
 // usersRoutes.post('/', authMiddleware, usersController.getAllUserIncidents);
 
